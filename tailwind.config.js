c35016b5e30386815a63c3da1e8494e082813e66
KAKHA13 <kakhagiorgashvili@gmail.com>
2025-11-30 19:48:56 +0400

    updated

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        "./index.html",
+        "./App.tsx",
+        "./index.tsx",
+        "./components/**/*.{js,ts,jsx,tsx}",
+        "./utils/**/*.{js,ts,jsx,tsx}",
+        "./constants.ts",
+        "./types.ts"
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
